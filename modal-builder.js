22173df8e7e484e1ba4ca216c7a093b8dfbd9c92
targyalo_a <targyalo@nemmindegy.hu>
2016-09-09 14:30:38 +0200

    probalkozom

--- modal-builder.js
+++ modal-builder.js
@@ -1,3 +1,11 @@
 /**
  * Created by kovacs.oszkar on 2016.08.19..
  */
+var ModalBuilder = function(id) {
+	this.id = id || "";
+	var form = document.getElementById(id);
+
+	form.onsubmit = function(){
+		var modalTitle = document.eg
+	};
+}
