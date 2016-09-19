fd02b68c16a05c27aa2a8f30f741891a22ae1dfd
Huszak Igor <huszak.igor@aion.hu>
2016-09-19 22:55:53 +0200

    modal builder

--- modal-builder.js
+++ modal-builder.js
@@ -1,65 +1,148 @@
 /**
- * Created by kovacs.oszkar on 2016.08.19..
+ * created by Igor
  */
 "use strict";
 var ModalBuilder = function(id) {
-	this.id = id || "";
-	var form = document.getElementById(id),
-	    formFields = {
-			"modalTitle": document.getElementById("modal-title"),
-			"modalBody": document.getElementById("modal-body"),
-			"modalBttns": document.getElementById("modal-buttons")
-	     },
-             validator = new Validator(formFields);
 
-	form.onsubmit = function(){
-		var modalContainer = document.createElement('div'),
-		    modalTitle = document.createElement('h2'),
-		    modalBody = document.createElement('p'),
-		    bttns = formFields.modalBttns.value.split(",");
+	/** define form and form elements */
+	var form = document.getElementById(id);
+	var formFields = {
+		'title': document.getElementById('modal-title'),
+		'body': document.getElementById('modal-body'),
+		'buttons': document.getElementById('modal-buttons')
+	};
+
+	/** initialize validator object */
+	var validator = new Validator(formFields);
+
+	/** form submit action */
+	form.onsubmit = function() {
+
 		if (validator.isValid() !== true) {
 			alert(validator.getErrorMessages());
 			return false;
 		}
 
-		modalContainer.appendChild(modalTitle);
-		modalContainer.appendChild(modalBody);
-		for(var i in bttns) {
-			let label = bttns[i].trim();
-			if(label.length){
-				var btn = document.createElement('button');
-				btn.innerText = label;
-				btn.type = "button";
-				modalContainer.appendChild(btn);
-			}
-		}
-
-		modalTitle.innerHTML = formFields.modalTitle.value;
-		modalBody.innerHTML = formFields.modalBody.value;
-
-		console.debug(modalContainer);
-
-		modalContainer.id = "modal-container";
-
+		/** initialize and render modal html */
+		var renderer = new Renderer(formFields);
+		renderer.toHtml();
 
 		return false;
+
 	};
-},
-Validator = function(fields) {
+
+};
+
+/** validator object */
+var Validator = function(fields) {
 	this.fields = fields || [];
 };
-Validator.prototype.errorMessages = [];		
-Validator.prototype.isValid = function(){
+
+
+Validator.prototype.errorMessages = [];
+
+/** validator validate function */
+Validator.prototype.isValid = function() {
+
 	this.errorMessages = [];
-	for (var i in this.fields) {	
-		let field = this.fields[i],
-		    label = field.previousElementSibling.innerText.replace(": ", "");		
+	for (var i in this.fields) {
+		var field = this.fields[i];
+		var label = field.previousElementSibling.innerText.replace(": ", "");
 		if (!field.value.length) {
 			this.errorMessages.push("Please fill: " + label);
-		}			
+		}
 	}
 	return this.errorMessages.length === 0;
+
 };
+
 Validator.prototype.getErrorMessages = function() {
 	return this.errorMessages.join("\n")
+};
+
+/** define renderer object */
+var Renderer = function(formFields) {
+	this.formElements = formFields || [];
+};
+
+/** renderer html function */
+Renderer.prototype.toHtml = function() {
+
+	/** get body html*/
+	var body = document.getElementsByTagName('body')[0];
+
+	/** create html elements */
+	var modalContainer = document.createElement('div');
+	var modalContent = document.createElement('div');
+	var modalHeader = document.createElement('div');
+	var modalTitle = document.createElement('h2');
+	var modalBody = document.createElement('div');
+	var modalFooter = document.createElement('div');
+	var modalClose = document.createElement('span');
+
+	/** get buttons from input */
+	var bttns = this.formElements.buttons.value.split(",");
+
+	/** added id/class to html elements */
+	modalContainer.id = 'modal';
+	modalContainer.className = 'modal';
+	modalContent.className = 'modal-content';
+	modalBody.className = 'modal-body';
+	modalHeader.className = 'modal-header';
+	modalFooter.className = 'modal-footer';
+	modalClose.className = 'close';
+
+	/**added inner text to close html element */
+	modalClose.innerText = 'x';
+
+	/** append close button and title to header html element */
+	modalHeader.appendChild(modalClose);
+	modalHeader.appendChild(modalTitle);
+
+	/** append header and body to content */
+	modalContent.appendChild(modalHeader);
+	modalContent.appendChild(modalBody);
+
+	for(var i in bttns) {
+
+		var label = bttns[i].trim();
+
+		if(label.length) {
+
+			/** create button html elemet(s) */
+			var btn = document.createElement('button');
+			btn.innerText = label.replace(/<(?:.|\n)*?>/gm, "");
+			btn.type = "button";
+
+			/** append button to footer element */
+			modalFooter.appendChild(btn);
+		}
+	}
+
+	/** added form values to html elements */
+	modalTitle.innerHTML = this.formElements.title.value.replace(/<(?:.|\n)*?>/gm, "");
+	modalBody.innerHTML = this.formElements.body.value.replace(/<(?:.|\n)*?>/gm, "");
+
+	/** append footer html element to content */
+	modalContent.appendChild(modalFooter);
+
+	/** append content to modal container */
+	modalContainer.appendChild(modalContent);
+	body.appendChild(modalContainer);
+
+	/** out of modal click function */
+	window.onclick = function(event) {
+		if (event.target == modalContainer) {
+			modalContainer.style.display = "none";
+			body.removeChild(modalContainer);
+		}
+	};
+
+	/** close button click function */
+	modalClose.onclick = function () {
+		modalContainer.style.display = "none";
+		body.removeChild(modalContainer);
+	};
+
+
 };
